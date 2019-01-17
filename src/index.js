cb50b5fa8f5ff5fc80259196eec1a3eab957432f
Sam Purcell <sam.purcell@soul-cycle.com>
2019-01-17 02:49:55 -0500

    Gets ApolloClient up and running with babel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+// Reqire dotenv first so you can keep some config variables secret
+// And you can use them in the rest of this file.
+require('dotenv').config();
+
+// Import cross-fetch since we are not in the brawser and apollo-client needs a fetch global.
+import 'cross-fetch/polyfill';
+
+// Import ApolloClient for the playground
 import ApolloClient from 'apollo-boost';
 
+// Create the apollo client with the placeholder url being the Github graphql endpoint.
 const client = new ApolloClient({
   url: 'https://api.github.com/graphql',
   request: operation => {
     operation.setContext({
       headers: {
-        authorization: `Bearer GITHUB_ACCESS_TOKEN`,
+        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
       },
     });
   },
