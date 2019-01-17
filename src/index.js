3e14b5842e9ff89d90d11043f3578805e2b6627a
Sam Purcell <sam.purcell@soul-cycle.com>
2019-01-17 15:59:29 -0500

    Finishes set up for ApolloClient on CLI with apollo-boost

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-// Reqire dotenv first so you can keep some config variables secret
-// And you can use them in the rest of this file.
+// Require dotenv first so you can keep some config variables secret,
+// and you can use them in the rest of this file.
 require('dotenv').config();
 
 // Import cross-fetch since we are not in the brawser and apollo-client needs a fetch global.
 import 'cross-fetch/polyfill';
 
-// Import ApolloClient for the playground
-import ApolloClient from 'apollo-boost';
+// Import ApolloClient and gql for the boilerplate
+import ApolloClient, { gql } from 'apollo-boost';
 
-// Create the apollo client with the placeholder url being the Github graphql endpoint.
+/** Queries */
+
+// This is an example to get the Github organization by login.
+const GET_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+
+// Create the apollo client.
+// The placeholder uri is the Github graphql endpoint.
 const client = new ApolloClient({
-  url: 'https://api.github.com/graphql',
+  uri: 'https://api.github.com/graphql',
   request: operation => {
     operation.setContext({
       headers: {
@@ -19,3 +32,16 @@ const client = new ApolloClient({
     });
   },
 });
+
+// ApolloClient makes the graphql query.
+// Under the hood it is still using an HTTP POST request.
+// It makes the request, and logs the result of the query on the command line,
+// or it logs the error.
+client
+  .query({
+    query: GET_ORGANIZATION,
+  })
+  .then(console.log)
+  .catch((e) => {
+    console.log(e);
+  });
